73990eae78b15d5ba87072ba48e9258869123fbc
Ossian Dillner <39240378+odillner@users.noreply.github.com>
2020-06-04 14:14:06 +0200

    added production build

---
+++ build/precache-manifest.94792e9a127ea5abd65dede8c0b33696.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ddb2ef0dd569ccb5c91000d01d709f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf634fd0bbb73cd8f91a",
+    "url": "/static/css/main.8d37bb24.chunk.css"
+  },
+  {
+    "revision": "a75cfe734ddb11b2e053",
+    "url": "/static/js/2.2381a9e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2381a9e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf634fd0bbb73cd8f91a",
+    "url": "/static/js/main.89f4a3a5.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3001
 const routes = require('./routes');
 
 //middleware
+app.use(express.static('build'))
 app.use(cors());
 app.use(express.json()); //body
 app.use(morgan('tiny')); //logger
